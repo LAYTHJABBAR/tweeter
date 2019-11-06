80d6c7ca1a65c80ce6e1045a3177b5c102beb377
LAYTHJABBAR <layth.jabbar@yahoo.com>
2019-11-06 22:37:24 +0000

    done

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,15 @@
-const data111 = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  }
-];
 
 //Function to create mark up of tweet.
 const createTweetElement = data => {
 
- return `<section id="tweetcontainer" class="head">
+ return `
   <article class='tweet'>
-    <div class="article-header"></div>
-    <div>
+    <div class="article-header">
       <img src="${data.user.avatars}">
       <h5 class="tweet-name">${data.user.name}</h5>
       <h3 class="tweet-name-right">${data.user.handle}</h3>
     </div>
-    <div class="texttweet">${data.content.text}</div><br/>
+    <div class="texttweet">${data.content.text}</div>
     <footer class="article-footer">
       <span class="tweet-time">${data.created_at} </span>
       <div class="tweet-icons">
@@ -44,37 +19,44 @@ const createTweetElement = data => {
       </div>
     </footer>
   </article>
-</section>`
+`
 };
 const renderTweets = data => {
+  $("#tweetcontainer").empty();
   data.forEach(el => {
     const tweet = createTweetElement(el);
-    $(".new-tweet").append(tweet);
+    $("#tweetcontainer").append(tweet);
   });
 };
+
+
+const loadTweets =  function () {
+  $.ajax({
+    type: "GET",
+    url: "/tweets",
+    success: (data1 => renderTweets(data1))
+  })
+}
 //Appending to the DOM after DOM is ready.
 $(document).ready(() => {
-  renderTweets(data);
+  // renderTweets(data);
+  
+  $( "form" ).submit(function( event ) {
+    let data1 = $(this).serialize();
+    console.log( "Handler sdfghjkmit() called.", $(this).serialize());
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: data1,
+      success: function() {
+        console.log("done");
+        loadTweets()
+      },
+    });
+  });
+
+  loadTweets()
 });
-$( "form" ).submit(function( event ) {
-  let data1 = $(this).serialize();
-  console.log( "Handler sdfghjkmit() called.", $(this).serialize());
-  event.preventDefault();
 
 
-$.ajax({
-  type: "POST",
-  url: "/tweets",
-  data: data1,
-  success: function(data1) {
-    console.log("done", data1);
-  },
-});
-$.ajax({
-  type: "GET",
-  url: "/tweets",
-  success: (data1 => renderTweets(data1))
-
- 
-})
-});
